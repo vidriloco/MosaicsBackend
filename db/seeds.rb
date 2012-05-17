a1dee90862bd7f6f2543a2d6ce3d8fa75f273f49
Alejandro Paz <vidriloco@gmail.com>
2012-05-17 17:31:13 -0500

    Restored index page for welcome controller index action

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create(:email => "admin@quantus.com", :password_confirmation => "password", :password => "password", :username => "quantusroot")
+AdminUser.create(:email => "admin@quantus.com", :password_confirmation => "quantus-ya", :password => "quantus-ya", :username => "quantusroot")
 Organization.create(:name => "Quantus")
 Device.create(:identifier => "A4:67:06:82:D0:B5")
-Pollster.create(:email => "pregunton@quantus.com", :password_confirmation => "pollita", :password => "pollita", :username => "leiden", :full_name => "Citizen Present")
-Pollster.create(:email => "preguntona@quantus.com", :password_confirmation => "pollita", :password => "pollita", :username => "antwerpen", :full_name => "Citizen Future")
+Pollster.create(:email => "leiden@quantus.com", :password_confirmation => "leiden", :password => "leiden", :username => "leiden", :full_name => "Leiden City")
